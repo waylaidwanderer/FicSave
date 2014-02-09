65f7a94fd743a772b60e97d11d5ee9b60cd71cff
waylaidwanderer <waylaidwanderer@live.com>
2014-02-09 14:06:12 +0000

    Updated config-example.php

--- config-example.php
+++ config-example.php
@@ -2,5 +2,6 @@
 $_CONFIG["username"] = ""; // SQL Username
 $_CONFIG["password"] = ""; // SQL Password
 $_CONFIG["host"] = ""; // Server IP or "localhost" if local server
+$_CONFIG["database"] = ""; // SQL Database name
 $_CONFIG["table"] = ""; // SQL table name
 ?>
