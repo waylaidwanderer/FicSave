5bbfffaa6a900ffa0dd9cba143cf789d63ecf41c
waylaidwanderer <waylaidwanderer@live.com>
2014-02-09 14:43:02 +0000

    Changed index.html to index.php for future usage

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php ini_set('max_input_time', 300); ini_set('max_execution_time', 300); set_time_limit(0); ?>
+<?php ini_set('max_input_time', 300); ?>
 <!doctype html>
 <html class="no-js" lang="en">
 <head>
