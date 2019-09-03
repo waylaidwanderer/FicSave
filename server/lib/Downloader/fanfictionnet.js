b8a1895419f9a273d5a2dde5c376c10f3a803c1a
Joel <waylaidwanderer@live.com>
2019-09-02 22:59:53 -0700

    Add server files

---
+++ server/lib/Downloader/fanfictionnet.js
@@ -0,0 +1,34 @@
+const Downloader = require('./index');
+
+class FanfictionNet extends Downloader {
+    constructor(url) {
+        super(url);
+        this.selectors.title = '#profile_top > b.xcontrast_txt';
+        this.selectors.author = '#profile_top > a.xcontrast_txt:nth-child(5)';
+        this.selectors.cover_art = '#img_large img';
+        this.selectors.body = '#storytext';
+    }
+
+    getChapters() {
+        const self = this;
+        const chapters = [];
+        this.$('#chap_select').first().find('option').each(function chapterValue() {
+            chapters.push(self.$(this).text().trim());
+        });
+        return chapters;
+    }
+
+    static getBaseUrl(url) {
+        const matches = /https:\/\/(.*)\/s\/(\d+)/.exec(url);
+        if (!matches) {
+            throw new Error('invalid URL');
+        }
+        return `https://www.fanfiction.net/s/${matches[2]}`;
+    }
+
+    getChapterUrl(chapterNumber) {
+        return `${this.url}/${chapterNumber}`;
+    }
+}
+
+module.exports = FanfictionNet;
---
+++ server/lib/Downloader/index.js
@@ -0,0 +1,68 @@
+const cheerio = require('cheerio');
+const axios = require('axios');
+
+const Epub = require('epub-gen');
+
+class Downloader {
+    constructor(url) {
+        this.url = this.constructor.getBaseUrl(url);
+        this.html = '';
+        this.selectors = {
+            title: null,
+            author: null,
+            summary: null,
+            metadata: null,
+            cover_art: null,
+            body: null,
+        };
+    }
+
+    /* eslint-disable */
+    getChapters() { }
+
+    static getBaseUrl(url) {
+        return url;
+    }
+
+    getChapterUrl(chapterNumber) {}
+    /* eslint-enable */
+
+    async download() {
+        const response = await axios.get(this.url);
+        this.html = response.data;
+        const $ = cheerio.load(this.html);
+        this.$ = $;
+        const data = {
+            title: $(this.selectors.title).first().text().trim(),
+            author: $(this.selectors.author).first().text().trim(),
+            publisher: this.url,
+            cover: this.selectors.cover_art ? $(this.selectors.cover_art).first().attr('src') : null,
+        };
+        if (data.cover.startsWith('//')) {
+            data.cover = `https:${data.cover}`;
+        }
+        data.output = `./tmp/${data.title} - ${data.author}.epub`;
+        const chapterList = this.getChapters();
+        const buildChaptersPromises = [];
+        chapterList.forEach((chapterTitle, index) => buildChaptersPromises.push(this.buildChapter(index + 1, chapterTitle)));
+        data.content = await Promise.all(buildChaptersPromises);
+        await (new Epub(data).promise);
+    }
+
+    async fetchChapter(chapterUrl) {
+        const response = await axios.get(chapterUrl);
+        const $ = cheerio.load(response.data);
+        return $(this.selectors.body).html();
+    }
+
+    async buildChapter(chapterNumber, chapterTitle) {
+        const chapterUrl = this.getChapterUrl(chapterNumber);
+        const chapterContent = await this.fetchChapter(chapterUrl);
+        return {
+            title: chapterTitle,
+            data: chapterContent,
+        };
+    }
+}
+
+module.exports = Downloader;
